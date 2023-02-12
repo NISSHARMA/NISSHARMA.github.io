7175f6afe14c4f7541896358fdd626d26bbe08f4
Nisha Sharma <112969807+NISSHARMA@users.noreply.github.com>
2023-02-12 14:59:02 +0530

    Portfolio

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function myfunction(){
+  console.log("res")
+  let x=document.getElementById("responsive_navbar")
+  if(x.style.display=="none"){
+    x.style.display="block"
+  }else{
+    x.style.display="none"
+  }
+}
