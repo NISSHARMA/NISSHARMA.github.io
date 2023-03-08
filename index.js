a80e2553a3116afb9639c94048100b79337856e0
NISSHARMA <nisha.01696@gmail.com>
2023-03-08 11:59:34 +0530

    Portfolio

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 
-function myfunction(){
-  console.log("res")
-  let x=document.getElementById("responsive_navbar")
-  if(x.style.display=="none"){
-    x.style.display="block"
-  }else{
-    x.style.display="none"
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
   }
 }
 
 
-function handleclose(){
-  let y=document.getElementById("responsive_navbar")
-    y.style.display="none"
-  
-}
-
 function openResume(){
   window.open(
     'https://drive.google.com/file/d/1Wpj4rOUpaMb5GvW60u-jds_l7IsifFx-/view?usp=share_link',
-    '_blank' // <- This is what makes it open in a new window.
+    '_blank' 
   );
 }
---
+++ portfolio.js
@@ -0,0 +1,17 @@
+
+/*function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+function openResume(){
+    window.open(
+      'https://drive.google.com/file/d/1Wpj4rOUpaMb5GvW60u-jds_l7IsifFx-/view?usp=share_link',
+      '_blank' 
+    );
+  }*/
