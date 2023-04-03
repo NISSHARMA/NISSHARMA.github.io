5d8c61cdc34232b01685bb6c46012c42ce285190
Nisha Sharma <Nisha Sharma>
2023-04-03 21:45:10 +0530

    Portfolio

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
 
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-      x.className += " responsive";
-  } else {
-      x.className = "topnav";
-  }
-}
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
+});
 
 
-function openResume(){
+function openResume() {
   window.open(
     'https://drive.google.com/file/d/1Wpj4rOUpaMb5GvW60u-jds_l7IsifFx-/view?usp=share_link',
-    '_blank' 
+    '_blank'
   );
 }
+
+function paddingFunction() {
+  let x = doccument.getElementById("home")
+  x.style.padding = "100px"
+}
+
+document.getElementById("submit").addEventListener("click", saveFile() );
+
+
+
+function saveFile(e) {
+
+  e.preventDefault()
+
+
+  let Formdata = JSON.parse(localStorage.getItem("portfolioData")) || []
+
+  let name = doccument.getElementById("fname")
+  let email = doccument.getElementById("email")
+  let text = document.getElementById("subject")
+  let data = {
+    "name": name.value,
+    "email": email.value,
+    "text": text.value
+  }
+  console.log(data)
+  Formdata.push(data)
+  localStorage.setItem("portfolioData", JSON.stringify(Formdata))
+}
+
+
+
